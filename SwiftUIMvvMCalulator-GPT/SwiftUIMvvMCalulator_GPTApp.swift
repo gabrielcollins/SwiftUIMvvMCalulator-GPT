86732be575dd113c7803e17f81f6977a24e9549a
Gabriel Alan Collins <little.seahorse@gmail.com>
2023-06-10 08:22:05 -0600

    Initial Commit

---
+++ SwiftUIMvvMCalulator-GPT/SwiftUIMvvMCalulator_GPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMvvMCalulator_GPTApp.swift
+//  SwiftUIMvvMCalulator-GPT
+//
+//  Created by Gabriel Alan Collins on 2023-06-10.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMvvMCalulator_GPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
